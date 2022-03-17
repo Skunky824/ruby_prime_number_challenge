c7cf0d91b88989a19af8689eb17a51614c8055b9
Skunky824 <79629191+Skunky824@users.noreply.github.com>
2022-03-17 08:11:33 +0100

    Add files via upload

---
+++ number.rb
@@ -0,0 +1,19 @@
+class Number
+  def initialize(n)
+    @n = n
+
+      #if number is 0 exit
+      while n == 0
+      p "Can't divide by 0, exiting.."
+      n +=1
+      return 0
+      end
+
+
+       arr = [*0..n]
+      arr.select { |x| x >= 2}.each do
+        p n / 5
+
+      end
+  end
+end
